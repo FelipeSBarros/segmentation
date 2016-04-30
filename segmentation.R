d32b7c0d52bdcb04c13a5616b901548f96afefd5
FelipeSBarros <felipe.b4rros@gmail.com>
2016-04-30 15:45:53 -0300

    Several improvements were done: 1. group simulation reduzed to 50 due processing capacity on large image; 2. Passos de processamento basico não existem mais; 3. Kmeans roda em imagens com NA, e considerando toda a imagem/; 4. set.seed implementado para reproducibilidade; 5. scale e nstrart=10 implementado no K-means; 6. save.shp parametro para indicar quando salvar analise em shp; 7. save.raster parametro para indicar quando salvar raster;

--- segmentation.R
+++ segmentation.R
@@ -2,62 +2,62 @@ segmentation <- function(envLayer = envLayer, #raster Layer or raster stack
                          studyArea = studyArea, # SpatialPolygonsDataFrame
                          projName = 'MT', # Sufix to be used when saving results
                          randomforest = TRUE,
-                         random.pt = NULL, # Number of random points to be genarated to run randomForest
                          Kmeans = TRUE,
+                         fuzzy.cluster = FALSE,
+                         random.pt = NULL, # Number of random points to be genarated to run randomForest
                          ngroup = NULL,
-                         polygonize = FALSE,
+                         save.shp = FALSE,
+                         save.raster = FALSE,
+                         save.plot=FALSE,
                          seed = 123) {
   library(rgdal)
   library(raster)
   library(rgeos)
   library(randomForest)
   library(ggplot2)
-  
-  # Basic procedures for all algorithms----
-  
-  #Generating random points:
-  if (is.null(random.pt)) {
-    random.pt <- as.integer(0.01 * ncell(envLayer))
-  }
-  cat("Generating", random.pt,"random points \n")
-  set.seed(seed)
-  sp.pts <- spsample(studyArea, random.pt, type = "random")
-  
-  #Extracting raster values for each point
-  sp.pts$prec <- extract(envLayer, sp.pts)
-  
-  #removing NA
-  if (any(is.na(sp.pts@data))) {
-    sp.pts <- sp.pts[-which(is.na(sp.pts@data)),]
-  }
-  
-  #creating object with the raster value to run kmeans
-  mydata <- as.data.frame(sp.pts@data)
-  
-  # Not used for this prupouse
-  #mydata <- scale(mydata) # standardize variables
-  
-  # Determine number of clusters
-  wss <- (nrow(mydata) - 1) * sum(apply(mydata,2,var))
-  
-  for (i in 2:(as.integer(0.01 * random.pt)))
-    wss[i] <- sum(kmeans(mydata, centers = i)$withinss)
-  wss2 <- as.data.frame(wss)
-  wss2$row <- as.integer(rownames(wss2))
-  
-  # Saving plot
-  ggplot(wss2, aes(x = row, y = wss)) + geom_line() + labs(x = "Number of Clusters", y = "Within groups sum of squares") + theme(text = element_text(size = 17))
-  ggsave(paste0("Kmeans_clusterAnalysis_",projName, ".png"), dpi = 300)
-  dev.off()
-  
-  plot(wss2$row, wss2$wss, xlab = "Number of Clusters", ylab = "Within groups sum of squares", type = 'b')
-  
-  if (is.null(ngroup)) {
-    # Number of classes to be classified
-    ngroup <- readline("How many groups should be created?")
-  }
-  
+
   if (randomforest) {
+    #Generating random points:
+    if (is.null(random.pt)) {
+      random.pt <- as.integer(0.01 * ncell(envLayer))
+    }
+    cat("Generating", random.pt,"random points \n")
+    set.seed(seed)
+    sp.pts <- spsample(studyArea, random.pt, type = "random")
+    
+    #Extracting raster values for each point
+    sp.pts$prec <- extract(envLayer, sp.pts)
+    
+    #removing NA
+    if (any(is.na(sp.pts@data))) {
+      sp.pts <- sp.pts[-which(is.na(sp.pts@data)),]
+    }
+    
+    #creating object with the raster value to run kmeans
+    mydata <- as.data.frame(sp.pts@data)
+    
+    # Not used for this prupouse
+    #mydata <- scale(mydata) # standardize variables
+    
+    # Determine number of clusters
+    wss <- (nrow(mydata) - 1) * sum(apply(mydata,2,var))
+    
+    for (i in 2:(as.integer(0.01 * random.pt)))
+      wss[i] <- sum(kmeans(mydata, centers = i)$withinss)
+    wss2 <- as.data.frame(wss)
+    wss2$row <- as.integer(rownames(wss2))
+    
+    # Saving plot
+    ggplot(wss2, aes(x = row, y = wss)) + geom_line() + labs(x = "Number of Clusters", y = "Within groups sum of squares") + theme(text = element_text(size = 17))
+    ggsave(paste0("Kmeans_clusterAnalysis_",projName, ".png"), dpi = 300)
+    dev.off()
+    
+    plot(wss2$row, wss2$wss, xlab = "Number of Clusters", ylab = "Within groups sum of squares", type = 'b')
+    
+    if (is.null(ngroup)) {
+      # Number of classes to be classified
+      ngroup <- readline("How many groups should be created?")
+    }
     # K-Means Cluster Analysis
     fit <- kmeans(
       mydata, ngroup, algorithm = c("Hartigan-Wong",
@@ -100,55 +100,114 @@ segmentation <- function(envLayer = envLayer, #raster Layer or raster stack
     cat("randomForest segmentation done. \n")
   }
   if (Kmeans) {
-    # It is not possible to run Dataset with NA
     mydata <- getValues(envLayer)
-    if (any(is.na(mydata))) {
-      cat('Raster data with NA values. \n')
+    
+    # Identificando pixel nao NA
+    a <- which(!is.na(mydata))
+    
+    # Creating data to Kmeans analysis
+    mydata <- na.omit(values(envLayer))
+    
+    # Not used for this prupouse
+    mydata <- scale(mydata) # standardize variables
+    
+    if(save.plot){  
       
-      # Removing NA from background
-      i <- which(!is.na(mydata))
+      # Determine number of clusters
+      wss <- (nrow(mydata) - 1) * sum(apply(mydata,2,var))
+    
+      for (b in 2:20)
+      wss[b] <- sum(kmeans(mydata, centers = i)$withinss)
+      wss2 <- as.data.frame(wss)
+      wss2$row <- as.integer(rownames(wss2))
       
-      # Creating data to Kmeans analysis
-      mydata <- na.omit(values(envLayer))
+      #plotting results
+      plot(wss2$row, wss2$wss, xlab = "Number of Clusters", ylab = "Within groups sum of squares", type = 'b')
       
-      # K-Means Cluster Analysis
-      fit <-
-        kmeans(
-          mydata, ngroup, algorithm = c("Hartigan-Wong",
-                                                          "Lloyd",
-                                                          "Forgy",
-                                                          "MacQueen")[1])
+      # Saving plot
+      ggplot(wss2, aes(x = row, y = wss)) + geom_line() + labs(x = "Number of Clusters", y = "Within groups sum of squares") + theme(text = element_text(size = 17))
+      ggsave(paste0("Kmeans_clusterAnalysis_",projName, ".png"), dpi = 300)
+      dev.off()
+    }
+    
+    if (is.null(ngroup)) {
+      # Number of classes to be classified
+      ngroup <- readline("How many groups should be created?")
+      }
       
-      # creating a raster layer to recieve goup values
-      km_SegmentationRaster <- envLayer[[1]]
-      
-      # changing pixel values to group values
-      km_SegmentationRaster[i] <- fit$cluster
-      
-      } else {
-      # K-Means Cluster Analysis
-      fit <-
-        kmeans(envLayer[], ngroup, algorithm = c("Hartigan-Wong",
-                                                 "Lloyd",
-                                                 "Forgy",
-                                                 "MacQueen")[1]) # n cluster solution
-      # creating a raster layer to recieve goup values
-      km_SegmentationRaster <- envLayer[[1]]
-      values(km_SegmentationRaster) <- fit$cluster}
-      
-      if (polygonize) {
+    # K-Means Cluster Analysis
+    set.seed(seed)
+    fit <-
+      kmeans(
+        mydata, ngroup, algorithm = c("Hartigan-Wong",
+                                      "Lloyd",
+                                      "Forgy",
+                                      "MacQueen")[1], nstart=10)
+    
+    # creating a raster layer to recieve goup values
+    km_SegmentationRaster <- envLayer[[1]]
+    
+    # changing pixel values to group values
+    km_SegmentationRaster[a] <- fit$cluster
+    
+    # plot(mydata[,c(7:7)], col=fit$cluster, pch='.')
+    # points(fit$centers[,c(1, 2)], col=1:3, pch=8, cex=2)
+    
+    if (save.shp) {
         # Saving Vector output (Polygon)
         km_SegmentationRaster <-
           rasterToPolygons(
             km_SegmentationRaster, fun = NULL, n = 4, na.rm = TRUE, digits = 12, dissolve =  FALSE)
         writeOGR(
           km_SegmentationRaster, dsn = './', layer = paste0('km_segmentation_',projName), driver = "ESRI Shapefile", overwrite_layer = TRUE)
-      } else {
+      } 
+    if (save.raster) {
         #Saving RASTER output
         cat("Saving raster result \n")
         writeRaster(
           km_SegmentationRaster, filename = paste0('./km_segmentation_',projName,'.tif'), overwrite = TRUE)
         }
     cat("Kmeans segmentation done. \n")
+  }
+  if (fuzzy.cluster){
+    library(e1071)
+    
+    mydata <- getValues(envLayer)
+    
+    # Identificando pixel nao NA
+    a <- which(!is.na(mydata))
+    
+    # Creating data to Kmeans analysis
+    mydata <- na.omit(values(envLayer))
+    
+    # Not used for this prupouse
+    mydata <- scale(mydata) # standardize variables
+    
+    set.seed(seed)
+    fuzzy <- cmeans(mydata, ngroup, iter.max = 100, verbose = FALSE,
+                    dist = c("euclidean", "manhattan")[1], method = c("cmeans","ufcl")[1], m = 2,
+                    rate.par = NULL, weights = 1, control = list())
+    
+    # creating a raster layer to recieve goup values
+    fuzzy_SegmentationRaster <- envLayer[[1]]
+    
+    # changing pixel values to group values
+    fuzzy_SegmentationRaster[a] <- fuzzy$cluster
+    
+    if (save.shp) {
+      # Saving Vector output (Polygon)
+      fuzzy_SegmentationRaster <-
+        rasterToPolygons(
+          fuzzy_SegmentationRaster, fun = NULL, n = 4, na.rm = TRUE, digits = 12, dissolve =  FALSE)
+      writeOGR(
+        fuzzy_SegmentationRaster, dsn = './', layer = paste0('fuzzy_segmentation_',projName), driver = "ESRI Shapefile", overwrite_layer = TRUE)
+    } 
+    if (save.raster) {
+      #Saving RASTER output
+      cat("Saving raster result \n")
+      writeRaster(
+        fuzzy_SegmentationRaster, filename = paste0('./fuzzy_segmentation_',projName,'.tif'), overwrite = TRUE)
     }
+    cat("Fuzzy segmentation done. \n")
+  }
   }
